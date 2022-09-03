20ab4f1f16e2df74696ea89f675eed2de1d1530b
nights192 <nights192@gmail.com>
2022-09-02 20:33:24 -0400

    Implemented automatic reconnection on tab enter.

--- frontend/src/hooks/use-socket.tsx
+++ frontend/src/hooks/use-socket.tsx
@@ -1,8 +1,24 @@
 import { useRef, useState, useEffect } from 'react';
 
+type InitSocketDelegate = (address: string, onMessage: (ws: WebSocket, event: MessageEvent) => void, initReconnect: number) => void;
+type OnMessageDelegate = (ws: WebSocket, event: MessageEvent) => void
+
 // Specifies the rate by which reconnection is spaced out on successive failures.
 function reconnectGrowth(x: number) {
     return x * 1.5;
+}
+
+// Schedules an attempt to reconnect with a websocket.
+function reconnect(
+    ws: WebSocket, address: string, onMessage: OnMessageDelegate,
+    initSocket: InitSocketDelegate, oldWait: number, newWait: number
+) {
+    setTimeout(() => {
+        if (ws.readyState !== WebSocket.CONNECTING ||
+                ws.readyState !== WebSocket.OPEN)
+
+            initSocket(address, onMessage, newWait);
+    }, oldWait);
 }
 
 export default function useSocket(address: string, onOpen: (ws: WebSocket, event: Event) => void, onMessage: (ws: WebSocket, event: MessageEvent) => void) {
@@ -12,7 +28,7 @@ export default function useSocket(address: string, onOpen: (ws: WebSocket, event
     useEffect(() => {
         //  We perform this operation as a destructive function so as to allow the socket to re-assign itself
         // in the case of failure.
-        function initSocket(address: string, onMessage: (ws: WebSocket, event: MessageEvent) => void, initReconnect: number = 1000, ) {
+        function initSocket(address: string, onMessage: (ws: WebSocket, event: MessageEvent) => void, initReconnect: number = 1000,) {
             ws.current = new WebSocket(address);
 
             function onOpenInternal(this: WebSocket, event: Event) {
@@ -22,7 +38,8 @@ export default function useSocket(address: string, onOpen: (ws: WebSocket, event
                 this.onclose = () => {
                     setConnected(false);
 
-                    setTimeout(() => initSocket(address, onMessage, 1000), 1000);
+                    if (ws.current !== null)
+                        reconnect(ws.current, address, onMessage, initSocket, 1000, reconnectGrowth(1000));
                 }
             }
 
@@ -31,17 +48,25 @@ export default function useSocket(address: string, onOpen: (ws: WebSocket, event
             }
 
             ws.current.onopen = onOpenInternal;
-
             ws.current.onmessage = onMessageInternal;
 
             ws.current.onclose = () => {
                 setConnected(false);
 
-                setTimeout(() => initSocket(address, onMessage, reconnectGrowth(initReconnect)), initReconnect);
+                if (ws.current !== null)
+                    reconnect(ws.current, address, onMessage, initSocket, initReconnect, reconnectGrowth(initReconnect));
             };
         }
 
+        const focusEvent = () => {
+            if (ws.current !== null)
+                reconnect(ws.current, address, onMessage, initSocket, 1000, reconnectGrowth(1000));
+        };
+
         if (ws.current === null) initSocket(address, onMessage);
+        window.addEventListener("focus", focusEvent);
+
+        return () => window.removeEventListener("focus", focusEvent);
     });
 
     return [ws, connected];
